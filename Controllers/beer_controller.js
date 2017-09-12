5ae7a6096e3504df1a60e71422144ed75bbfeecc
reneegeffre <renee.geffre@gmail.com>
2017-09-12 17:14:50 -0400

    route updates

--- Controllers/beer_controller.js
+++ Controllers/beer_controller.js
@@ -6,31 +6,48 @@ var passport = ('../config/passport');
 //Importing the models to use for database functions
 var user = require('../models/user.js');
 
+//Creating routes to render pages
 
-//Create all the required routes and logic
 router.get('/', function(req, res) {
-  user.all(function(data) {
-    varhbsObject = {
-      users: data
-    };
-    console.log(hbsObject);
+  res.render('index');
+});
+
+router.post("/signup", function(req, res) {
+  console.log(req.body);
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password
+  }).then(function() {
+    res.redirect(307, "/login");
+  }).catch(function(err) {
+    console.log(err);
+    res.json(err);
+    // res.status(422).json(err.errors[0].message);
   });
 });
 
-router.post('/', function(req, res) {
-  user.create([
-      'username', 'favorites'
-  ], [req.body.username, req.body.favorites
-  ], function() {
-    res.redirect('/');
-  });
+// Route for logging user out
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-router.put('/:id', function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log('condition', condition);
+// Route for getting some data about our user to be used client side
+router.get("/user_data", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  }
+  else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    });
+  }
 });
+
 
 //Export routes for app.js to use
 module.exports = router;

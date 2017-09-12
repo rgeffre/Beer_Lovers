469e81e3f848866e63e5bd3f295287e60d0f6235
reneegeffre <renee.geffre@gmail.com>
2017-09-12 11:41:38 -0400

    updates

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 var express = require('express');
+var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 //requiring passport as it's been configured for this app
-var passport = require('.config/passport');
+var passport = require('./config/passport');
 
 //Configuring the port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require('./models');
 
-
+//Configuring express app
+var app = express();
 
 // view engine setup
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//Configuring express app and middleware
-var app = express();
+//Configuring middleware parameters
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 //Setting up sessions to track user login status
 app.use(session({secret:"keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 //Import routes
-var routes = require ('./config/beer_controller.js');
+var routes = require ('./controllers/beer_controller.js');
 
 app.use('/', routes);
 
 
 //Syncing the database and logging a success message
-db.deqelize.snc().then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });

bacb916fb43254a01cb750434bcba0e04497098a
reneegeffre <renee.geffre@gmail.com>
2017-09-13 14:21:27 -0400

    project changes

--- Controllers/beer_controller.js
+++ Controllers/beer_controller.js
@@ -21,8 +21,8 @@ router.get('/login', function(req, res) {
   res.render('login');
 });
 
-router.get('/pub', function(req, res) {
-  res.render('pub');
+router.get('/mypub', function(req, res) {
+  res.render('mypub');
 });
 
 router.get('/search', function(req, res) {
@@ -49,6 +49,18 @@ router.get('/searchBeer', function (req, res) {
     res.render('search', { body: jbody });
   });
 });
+
+module.exports = function(app) {
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/members");
+  });
+
 
 router.get('/signup', function(req, res) {
   res.render('signup');
--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -1,4 +1,6 @@
-// Requiring our models and passport as we've configured it
+'use strict';
+
+var path = require('path');
 var db = require("../models");
 var passport = require("../config/passport");
 
@@ -50,6 +52,34 @@ module.exports = function(app) {
         id: req.user.id
       });
     }
+  });
+
+};
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
 };

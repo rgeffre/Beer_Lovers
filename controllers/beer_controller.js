aa833d90bde069db1cf372f7c6f9e98904edca55
reneegeffre <renee.geffre@gmail.com>
2017-09-17 20:23:14 -0400

    AR's map and event updates

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -1,4 +1,5 @@
 //Requiring dependencies
+'use strict';
 var express = require('express');
 var router = express.Router();
 var passport = ('../config/passport');
@@ -9,14 +10,20 @@ var user = require('../models');
 
 //Creating routes to render pages
 
-router.get('/', function(req, res) {
-  res.render('index');
-});
-
 router.get('/blog', function(req, res) {
   res.render('blog');
 });
 
+router.get('/', function(req, res) {
+  request('http://api.brewerydb.com/v2/events?key=0d40d88d9c4811140db6bf54d6d5f282&year=2017', function (error, response, body) {
+
+    var result = JSON.parse(body);
+    var results = result.data;
+    console.log(result.data);
+
+    res.render('index',{results});
+  })
+});
 
 //Export routes for app.js to use
 module.exports = router;
